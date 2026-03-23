ad95877fe54a2f378ba1ffa5e03a187c98cccd3c
cangle007 <cang.b.le@gmail.com>
2026-03-23 00:12:09 -0700

    fix server code

--- tests/integration/reportFlow.test.js
+++ tests/integration/reportFlow.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../app');
+const app = require('../../server');
 const { knex, resetDatabase } = require('./setupTestDb');
 const createAdminUser = require('./helpers/createAdminUser');
 
